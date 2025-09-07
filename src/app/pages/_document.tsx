786cc73aeed19efdbfbdbf9c884eafc401d0c80a
Ashraf Qopiah <ashraf@elmotech.it>
2025-09-07 18:13:45 +0300

    x

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -2,10 +2,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ar" dir="rtl">
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
+        <meta name="application-name" content="My Dashboard PWA" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Dashboard" />
+        <meta name="description" content="My Dashboard PWA Application" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <link rel="apple-touch-icon" href="/next.svg" />
+        <link rel="icon" type="image/svg+xml" href="/next.svg" />
+        <link rel="shortcut icon" href="/next.svg" />
       </Head>
       <body>
         <Main />
